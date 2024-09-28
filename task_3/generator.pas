ae2e906e5f42a98721dac49e7080a6bd865e1aed
HeatleesFlame <Snoopdogretoo@gmail.com>
2024-09-28 22:03:16 +0700

    array generator done

--- task_3/generator.pas
+++ task_3/generator.pas
@@ -1,20 +1,26 @@
 unit generator;
 
 interface
- function Generate: Int64;
-implementation
+  type
+    IntegerArray =  array of int32;
+  
+ Function Generate(len, max_num: int32): IntegerArray;
 
-  function Generate(len, max_num: int32) : array of int32;
+implementation
+  function Generate(len, max_num: int32) : IntegerArray;
     var
-      arr: array of int32;
+      arr: IntegerArray;
       i: int32; 
     begin
       SetLength(arr, len);
-      for i:= 1 to len do
+      for i:= 0 to len-1 do
         begin
           arr[i]:= random(max_num)
         end;
+      Generate:= arr;
+      exit;
     end;
+
 
   initialization
    Randomize();
